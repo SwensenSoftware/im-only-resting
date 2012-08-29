6c82908b9021d235a8f28e79a71c43a53e48183f
sswen4sa <sswen4sa@034d9c11-a720-b14a-d1cd-892a666db614>
2012-08-29 02:26:25 +0000

    fix it

--- RestSharpGui/trunk/RestSharpGui/MainForm.Designer.cs
+++ RestSharpGui/trunk/RestSharpGui/MainForm.Designer.cs
@@ -417,6 +417,7 @@
             // 
             // MainForm
             // 
+            this.AcceptButton = this.btnSubmitRequest;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1703, 551);
