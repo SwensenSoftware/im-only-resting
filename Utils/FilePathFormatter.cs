b0e54d9ab280c5869be4c347ea2a533191adaa94
stephen.swensen <stephen.swensen@gmail.com>
2012-09-20 13:14:01 +0000

    refactor

--- Utils/FilePathFormatter.cs
+++ Utils/FilePathFormatter.cs
@@ -3,10 +3,10 @@ using System.IO;
 
 namespace Swensen.Utils {
     public enum FilePathFormat {
-        Full, Short, Ellipsis, ShortFileFullDir
+        Full, Short, Ellipsis, ShortFileFullDir, FullDir
     }
 
-    public static class FilePathFormater {
+    public static class FilePathFormatter {
         public static string Format(this FileInfo filePath, FilePathFormat format) {
             switch (format) {
                 case FilePathFormat.Full:
@@ -17,6 +17,8 @@ namespace Swensen.Utils {
                     return string.Format("{0} ({1})", filePath.Name, filePath.DirectoryName);
                 case FilePathFormat.Ellipsis:
                     return string.Format(@"{1}...{0}{2}{0}{3}", Path.DirectorySeparatorChar, filePath.Directory.Root.Name, filePath.Directory.Name, filePath.Name);
+                case FilePathFormat.FullDir:
+                    return filePath.DirectoryName;
                 default:
                     throw new ArgumentOutOfRangeException("format");
             }
