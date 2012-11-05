aded33311b805136f14b914ade3dbf38066631ba
stephen.swensen <stephen.swensen@gmail.com>
2012-11-05 01:15:55 +0000

    refactoring

--- Ior/Forms/Program.cs
+++ Ior/Forms/Program.cs
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
 using NLog;
 using System.Reflection;
--- Utils/HistoryList.cs
+++ Utils/HistoryList.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Swensen.Utils {
     public class HistoryList<T> : IEnumerable<T> {
