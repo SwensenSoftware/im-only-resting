aea6a5e3b3f5eea866b24aa33acdc4f57fef909b
stephen.swensen@gmail.com <stephen.swensen@gmail.com@034d9c11-a720-b14a-d1cd-892a666db614>
2014-06-23 02:26:26 +0000

    clean up

--- trunk/Ior/Core/RequestModel.cs
+++ trunk/Ior/Core/RequestModel.cs
@@ -19,8 +19,6 @@ using System.Globalization;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Reflection;
-using System.Text;
-using System.Net;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using Swensen.Utils;
