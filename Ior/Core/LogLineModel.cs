7b231e1d0eb30119f7def9b779ecbcc70a27e72d
stephen.swensen <stephen.swensen@gmail.com>
2014-07-27 03:55:16 +0000

    refactoring

---
+++ Ior/Core/LogLineModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLog;
+
+namespace Swensen.Ior.Core
+{
+    public class LogLineModel {
+        public LogLineModel(string text) {
+            this.Text = text;
+            this.Level = LogLevel.Off;
+        }
+
+        public string Text {get; private set;}
+        public LogLevel Level {get; private set;}
+    }
+
+}
