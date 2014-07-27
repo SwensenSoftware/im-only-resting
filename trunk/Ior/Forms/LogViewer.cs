0ce1c3c223aea8dd034da1d165c6ca3fa86bbf5b
stephen.swensen@gmail.com <stephen.swensen@gmail.com@034d9c11-a720-b14a-d1cd-892a666db614>
2014-07-27 03:55:16 +0000

    refactoring

--- trunk/Ior/Forms/LogViewer.cs
+++ trunk/Ior/Forms/LogViewer.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
+using NLog;
+using Swensen.Ior.Core;
 
 namespace Swensen.Ior.Forms
 {
     public partial class LogViewer : Form
     {
+        private List<LogLineModel> logLines = new List<LogLineModel>();
+        private string logFilePath = String.Format("logs{0}log.txt", Path.DirectorySeparatorChar);
+
         public LogViewer()
         {
             InitializeComponent();
@@ -19,23 +24,34 @@ namespace Swensen.Ior.Forms
 
         private void LogViewer_Load(object sender, EventArgs e)
         {
+            readLogLines();
+            writeLogLines();
+        }
+
+        private void readLogLines() {
             try { 
                 //can't use File.ReadAllText since file is locked by NLog
-                using (var fileStream = new FileStream(@"logs/log.txt", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using (var fileStream = new FileStream(logFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
                 using (var textReader = new StreamReader(fileStream, Encoding.UTF8))
                 {
-                    txtLogViewer.SuspendReadonly(() => {
-                        while (!textReader.EndOfStream) {
-                            var content = textReader.ReadLine();
-                            txtLogViewer.InsertText(0, content + System.Environment.NewLine);
-                        }
-                    });
+                    while (!textReader.EndOfStream) {
+                        var content = textReader.ReadLine();
+                        logLines.Add(new LogLineModel(content + Environment.NewLine));
+                    }
                 }
             }
             catch {
-                MessageBox.Show(@"Could not load logs\log.txt, the file may not exist", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show(String.Format("Could not load {0}, the file may not exist", logFilePath), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 this.Close();
             }
+        }
+
+        private void writeLogLines() {
+            txtLogViewer.SuspendReadonly(() => {
+                foreach(var ll in logLines) {
+                    txtLogViewer.InsertText(0, ll.Text);
+                }
+            });
         }
     }
 }
