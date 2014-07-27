002e9891a4f5b29bd72d36ab1252b75859e7e0d1
stephen.swensen <stephen.swensen@gmail.com>
2014-07-27 04:01:28 +0000

    update assembly info

--- Ior/Properties/AssemblyInfo.cs
+++ Ior/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("I'm Only Resting")]
-[assembly: AssemblyDescription("http://code.google.com/p/im-only-resting/")]
+[assembly: AssemblyDescription("http://www.swensensoftware.com/im-only-resting")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("I'm Only Resting")]
-[assembly: AssemblyCopyright("Copyright © Stephen Swensen 2012-2013")]
+[assembly: AssemblyCopyright("Copyright © Swensen Software 2012 - 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
