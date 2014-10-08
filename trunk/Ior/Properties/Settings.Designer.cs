dbac3c0bf8ad5b43cc172bdc85f466f1079f6428
stephen.swensen@gmail.com <stephen.swensen@gmail.com@034d9c11-a720-b14a-d1cd-892a666db614>
2014-10-08 04:47:35 +0000

    issue 61 add setting for ignoring ssl validation errors

--- trunk/Ior/Properties/Settings.Designer.cs
+++ trunk/Ior/Properties/Settings.Designer.cs
@@ -202,5 +202,17 @@ namespace Swensen.Ior.Properties {
                 this["LogViewerLevelFilter"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool IgnoreSSLValidationErrors {
+            get {
+                return ((bool)(this["IgnoreSSLValidationErrors"]));
+            }
+            set {
+                this["IgnoreSSLValidationErrors"] = value;
+            }
+        }
     }
 }
